d2bf797690bd149225c23f15eccb16f0358ea67b
prakharshrestha <155898103+prakharshrestha@users.noreply.github.com>
2026-07-21 16:42:45 +0530

    0239-sliding-window-maximum Array Hard .java 30 ms (74.91%) 142.7 MB (97.91%) 07-21-2026

---
+++ LeetCode/Hard/0239-sliding-window-maximum/0239-sliding-window-maximum-07-21-2026-16-42-44.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> dq=new ArrayDeque<>();
+        int n=nums.length;
+
+        int[] ans=new int[n-k+1];
+        int index=0;
+
+        for(int i=0;i<n;i++){
+
+            while(!dq.isEmpty() && dq.peekFirst()<=i-k)
+            dq.pollFirst();
+
+            while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[i])
+            dq.pollLast();
+
+            dq.offerLast(i);
+
+            if(i>=k-1){
+            ans[index]=nums[dq.peekFirst()];
+            index++;
+            }
+        }
+        return ans;
+    }
+}
