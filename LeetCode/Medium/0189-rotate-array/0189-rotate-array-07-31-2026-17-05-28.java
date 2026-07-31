6d89b1a27d69080c3bccd40558f56b761c8ba62b
prakharshrestha <155898103+prakharshrestha@users.noreply.github.com>
2026-07-31 17:05:28 +0530

    0189-rotate-array Array Medium .java 0 ms (100%) 61.2 MB (71.46%) 07-31-2026

---
+++ LeetCode/Medium/0189-rotate-array/0189-rotate-array-07-31-2026-17-05-28.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        //Optimal O(n)
+        int n=nums.length;
+        k=k%n;
+        reverse(nums,0,n-1);
+        reverse(nums,k,n-1);
+        reverse(nums,0,k-1);
+    }
+    public void reverse(int[] nums,int l,int r){
+        int n=nums.length;
+        
+        while(l<r){
+            int temp=nums[l];
+            nums[l]=nums[r];
+            nums[r]=temp;
+
+            l++;
+            r--;
+        }
+
+    }
+}
