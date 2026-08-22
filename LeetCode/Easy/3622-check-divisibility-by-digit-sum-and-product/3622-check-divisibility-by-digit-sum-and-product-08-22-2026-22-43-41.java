93d5f2142f9d319cbbbe35ae600dcda8b357100c
prakharshrestha <155898103+prakharshrestha@users.noreply.github.com>
2026-08-22 22:43:41 +0530

    3622-check-divisibility-by-digit-sum-and-product Math Easy .java 0 ms (100%) 42.6 MB (24.68%) 08-22-2026

---
+++ LeetCode/Easy/3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product-08-22-2026-22-43-41.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int sum=0;
+        int prod=1;
+        int num=n;
+        while(num>0)
+        {
+            int d=num%10;
+            sum+=d;
+            prod*=d;
+            num=num/10;
+        }
+        if(n%(sum+prod)==0)
+        return true;
+
+        return false;
+    }
+}
