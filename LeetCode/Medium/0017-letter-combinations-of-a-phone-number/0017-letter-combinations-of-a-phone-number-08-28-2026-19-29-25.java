83268d469b072bf2e49e12b97e7c9cd40d185300
prakharshrestha <155898103+prakharshrestha@users.noreply.github.com>
2026-08-28 19:29:25 +0530

    0017-letter-combinations-of-a-phone-number Hash Table Medium .java 4 ms (5.69%) 49.1 MB (10.94%) 08-28-2026

---
+++ LeetCode/Medium/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number-08-28-2026-19-29-25.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+
+        if (digits.length() == 0) return ans;
+
+        HashMap<Integer, String> keys = new HashMap<>();
+
+        keys.put(2, "abc");
+        keys.put(3, "def");
+        keys.put(4, "ghi");
+        keys.put(5, "jkl");
+        keys.put(6, "mno");
+        keys.put(7, "pqrs");
+        keys.put(8, "tuv");
+        keys.put(9, "wxyz");
+
+        solve(ans, digits, keys, 0, "");
+        return ans;
+    }
+    public void solve(List<String> ans, String digits,HashMap<Integer, String> keys,int ind, String str) {
+
+        if (ind == digits.length()) {
+            ans.add(str);
+            return;
+        }
+        int digit = digits.charAt(ind) - '0';
+        String letters = keys.get(digit);
+        for (int i = 0; i < letters.length(); i++) {
+            solve(ans, digits, keys, ind + 1,str + letters.charAt(i));
+        }
+    }
+}
