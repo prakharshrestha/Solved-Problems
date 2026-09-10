7cfb07870dd6e36b0c58fb94e0fdfaf3ce1c5d5e
prakharshrestha <155898103+prakharshrestha@users.noreply.github.com>
2026-09-10 17:17:44 +0530

    0016-3sum-closest Array Medium .java 17 ms (77.46%) 45.6 MB (49.31%) 09-10-2026

---
+++ LeetCode/Medium/0016-3sum-closest/0016-3sum-closest-09-10-2026-17-17-44.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int ans = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < n - 2; i++) {
+            int l = i + 1;
+            int r = n - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (Math.abs(target - sum) < Math.abs(target - ans)) {
+                    ans = sum;
+                }
+                if (sum < target) {
+                    l++;
+                } 
+                else if (sum > target) {
+                    r--;
+                } 
+                else {
+                    return sum;
+                }
+            }
+        }
+        return ans;
+    }
+}
