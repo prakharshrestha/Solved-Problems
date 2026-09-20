f83bc14a1edec18b9bde480f84eabced457e402f
prakharshrestha <155898103+prakharshrestha@users.noreply.github.com>
2026-09-20 16:33:52 +0530

    3498-reverse-degree-of-a-string String Easy .java 1 ms (100%) 43.9 MB (67.44%) 09-20-2026

---
+++ LeetCode/Easy/3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string-09-20-2026-16-33-52.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseDegree(String s) {
+
+        int ans = 0;
+        for(int i = 0; i < s.length(); i++) {
+            int reverse = 26 - (s.charAt(i) - 'a');
+            ans += reverse * (i + 1);
+        }
+        return ans;
+    }
+}
